48162cc18ced123cb10220c003f54f999d600a54
Amadou Sene <seneamadou996@gmail.com>
2020-11-30 22:55:02 +0000

    login done

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -3,16 +3,17 @@
 <div class="container custom-login">
     <div class="row">
         <div class="col-sm-4 col-sm-offset-4">
-            <form action="">
+            <form action="login" method="POST">
                 <div class="form-group">
-                    <label for="">Email adresse</label>
-                    <input type="text" class="form-control" id="" placeholder="">
+                        @csrf
+                    <label for="email">Email adresse</label>
+                    <input type="text" name="email" class="form-control" id="email" placeholder="email">
                 </div>
                 <div class="form-group">
-                    <label for="">Password</label>
-                    <input type="password" class="form-control" id="" placeholder="">
+                    <label for="password">Password</label>
+                    <input type="password" name="password" class="form-control" id="password" placeholder="password">
                 </div>
-                <button class="btn btn-default" type="submit">Submit</button>
+                <button class="btn btn-default" type="submit">Login</button>
             </form>
         </div>
     </div>
